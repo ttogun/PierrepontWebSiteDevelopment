419b7ee935c039b63c76846b49a4e679b651e5a1
NoMongoose <mdhuggins@optonline.net>
2014-10-23 20:57:12 -0400

    Small Cleanup

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $(document).ready( $(".sidebarLink").hover(function(){
-                                           
-                                           
+
+
                                            if($(this).hasClass("activeLink")){}
                                               else{
                                                 //$(".activeLink").animate({"padding-left": "0"});
                                                 $(this).animate({"padding-left": "1.5em"});
                                               }
-                                           
-                                           
+
+
                                            },
                                            function(){
                                               if($(this).hasClass("activeLink")){}
@@ -15,5 +15,5 @@ $(document).ready( $(".sidebarLink").hover(function(){
                                                  //$(".activeLink").animate({"padding-left": "1.5em"});
                                                  $(this).animate({"padding-left": "0"});
                                                  }
-                                    
+
                                            }));
