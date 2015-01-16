48a9a97ec62c698af323478fe1201f32685d2cfc
mdhuggins <mdhuggins@optonline.net>
2015-01-16 11:07:08 -0500

    Alumni Page Refinement

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -303,7 +303,7 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
   Carousel.VERSION  = '3.2.0'
 
   Carousel.DEFAULTS = {
-    interval: 5000,
+    interval: 0,
     pause: 'hover',
     wrap: true
   }
